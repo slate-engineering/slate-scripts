d794180fd5d28b1cead0ddb361ccb2fcf8eef557
toast <toastts@icloud.com>
2021-04-08 18:46:19 -0700

    changed upload method

--- scripts/upload_directory.py
+++ scripts/upload_directory.py
@@ -7,7 +7,7 @@ url = "https://uploads.slate.host/api/public/8eb2d471-9abf-4eae-a461-c62ebeb529b
 headers = {"Authorization": "Basic SLA6f6a789f-0031-4098-b037-c3d9f6e514c0TE"}
 
 # get dir to upload from
-direc = "/Users/toast/Downloads/movie_bladerunner"
+direc = "/Users/toast/my-docs/bookz"
 
 # array of files in direc
 upload_files = [i for i in os.listdir(direc)]
@@ -16,11 +16,9 @@ files_field = {}
 # iterate over files in directory, stream upload to slate
 for f in upload_files:
     file_path = os.path.join(direc, f)
-    files_field[f] = (f, open(file_path, "rb"))
+    if f != ".DS_Store":
+        m = MultipartEncoder(fields={"file": (f, open(file_path, "rb"))})
+        headers["Content-Type"] = m.content_type
+        r = requests.post(url, data=m, headers=headers)
+        print(r.status_code)
 
-files_field.pop(".DS_Store")
-m = MultipartEncoder(fields=files_field)
-headers["Content-Type"] = m.content_type
-
-r = requests.post(url, data=m, headers=headers)
-print(r.status_code)
